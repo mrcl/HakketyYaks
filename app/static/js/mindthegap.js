22aa40c69bb7266b9487a0482171e9a5c10d8483
Hamish Fraser <hamish@verb.co.nz>
2017-07-30 14:49:28 +1200

    now with data

--- app/static/js/mindthegap.js
+++ app/static/js/mindthegap.js
@@ -1,7 +1,12 @@
+var marginStacked = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 600 - marginStacked.left - marginStacked.right,
+    height = 500 - marginStacked.top - marginStacked.bottom;
+
+
 $( document ).ready(function() {
 
     $('#goaldetail').hide();
-    $.get( "/static/data/mindthegap.json", function( data ) {
+    $.get( "/static/data/mindthegap/mindthegap.json", function( data ) {
         $.mindthegap.thegoals = data.Goals;
         $.mindthegap.loadgoals();
     });
@@ -34,24 +39,25 @@ $.mindthegap = {
             }
         }
         
-        var stOut = '<h2>'+theGoal.Number+': '+theGoal.Name+'</h2>';
+        var stOut = '<h2>'+theGoal.Number+': '+theGoal.Name+'</h2><div id="chart"></div>';
 
         $('#goaldetail').html(stOut);
         $('#goaldetail').fadeIn();
-
+        $('#goaldetail').append($.mindthegap.rendergraph(theGoal));
         $('.goal').mouseleave();
+
         //});
     },
     rendergraph : function(theGoal){
-        /*
-        var svg = d3.select("svg"),
-            margin = {top: 20, right: 20, bottom: 30, left: 40},
-            width = +svg.attr("width") - margin.left - margin.right,
-            height = +svg.attr("height") - margin.top - margin.bottom,
-            g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var svg = d3.select("#chart").append("svg")
+            .attr("width", width + marginStacked.left + marginStacked.right)
+            .attr("height", height + marginStacked.top + marginStacked.bottom);
+
+        
+        g = svg.append("g").attr("transform", "translate(" + marginStacked.left + "," + marginStacked.top + ")");
 
         var x = d3.scaleBand()
-            .rangeRound([0, width])
+            .rangeRound([0, width-140])
             .paddingInner(0.05)
             .align(0.1);
 
@@ -59,75 +65,76 @@ $.mindthegap = {
             .rangeRound([height, 0]);
 
         var z = d3.scaleOrdinal()
-            .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+            .range([ "#6b486b", "#a05d56", "#d0743c", "#EAEAEA","#EAEAEA", "#8a89a6", "#7b6888"]);
 
-        d3.json("data.csv", function(d, i, columns) {
-        for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
-        d.total = t;
-        return d;
+        d3.csv("/static/data/mindthegap/"+theGoal.Number+".csv", function(d, i, columns) {
+            for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
+            d.total = t;
+            return d;
         }, function(error, data) {
-        if (error) throw error;
+            if (error) throw error;
 
-        var keys = data.columns.slice(1);
+            var keys = data.columns.slice(1);
 
-        data.sort(function(a, b) { return b.total - a.total; });
-        x.domain(data.map(function(d) { return d.State; }));
-        y.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
-        z.domain(keys);
+            //data.sort(function(a, b) { return b.total - a.total; });
+            x.domain(data.map(function(d) { return d.Gap; }));
+            y.domain([0, d3.max(data, function(d) { return d.total; })]).nice();
+            z.domain(keys);
 
-        g.append("g")
-            .selectAll("g")
-            .data(d3.stack().keys(keys)(data))
-            .enter().append("g")
-            .attr("fill", function(d) { return z(d.key); })
-            .selectAll("rect")
-            .data(function(d) { return d; })
-            .enter().append("rect")
-            .attr("x", function(d) { return x(d.data.State); })
-            .attr("y", function(d) { return y(d[1]); })
-            .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-            .attr("width", x.bandwidth());
+            g.append("g")
+                .selectAll("g")
+                .data(d3.stack().keys(keys)(data))
+                .enter().append("g")
+                .attr("fill", function(d) { return z(d.key); })
+                .selectAll("rect")
+                .data(function(d) { return d; })
+                .enter().append("rect")
+                .attr("x", function(d) { return x(d.data.Gap); })
+                .attr("y", function(d) { return y(d[1]); })
+                .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+                .attr("width", x.bandwidth());
 
-        g.append("g")
-            .attr("class", "axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+            g.append("g")
+                .attr("class", "axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x));
 
-        g.append("g")
-            .attr("class", "axis")
-            .call(d3.axisLeft(y).ticks(null, "s"))
-            .append("text")
-            .attr("x", 2)
-            .attr("y", y(y.ticks().pop()) + 0.5)
-            .attr("dy", "0.32em")
-            .attr("fill", "#000")
-            .attr("font-weight", "bold")
-            .attr("text-anchor", "start")
-            .text("Population");
+            g.append("g")
+                .attr("class", "axis")
+                .call(d3.axisLeft(y).ticks(null, "s"))
+                .append("text")
+                .attr("x", 2)
+                .attr("y", y(y.ticks().pop()) + 0.5)
+                .attr("dy", "0.32em")
+                .attr("fill", "#000")
+                .attr("font-weight", "bold")
+                .attr("text-anchor", "start")
+                .text("$");
 
-        var legend = g.append("g")
-            .attr("font-family", "sans-serif")
-            .attr("font-size", 10)
-            .attr("text-anchor", "end")
-            .selectAll("g")
-            .data(keys.slice().reverse())
-            .enter().append("g")
-            .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+            var legend = g.append("g")
+                .attr("font-family", "sans-serif")
+                .attr("font-size", 10)
+                .attr("text-anchor", "end")
+                .selectAll("g")
+                .data(keys.slice().reverse())
+                .enter().append("g")
+                .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-        legend.append("rect")
-            .attr("x", width - 19)
-            .attr("width", 19)
-            .attr("height", 19)
-            .attr("fill", z);
+            legend.append("rect")
+                .attr("x", width - 19)
+                .attr("width", 19)
+                .attr("height", 19)
+                .attr("fill", z);
 
-        legend.append("text")
-            .attr("x", width - 24)
-            .attr("y", 9.5)
-            .attr("dy", "0.32em")
-            .text(function(d) { return d; });
+            legend.append("text")
+                .attr("x", width - 24)
+                .attr("y", 9.5)
+                .attr("dy", "0.32em")
+                .text(function(d) { return d; });
         });
-*/
-
+            
+        
     }
 
-}
+};
+
